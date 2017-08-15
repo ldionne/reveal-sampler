439650dc55c67cfc728693258bd4a69987a6106d
Thomas Weinert <thomas@weinert.info>
2017-08-15 17:03:46 +0200

    Refactored, remove jQuery dependency

--- sampler.js
+++ sampler.js
@@ -15,49 +15,74 @@
         return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
     };
 
-    if (typeof $ === 'undefined') {
-        throw 'The sampler.js plugin for reveal.js requires jQuery to be loaded.';
-    }
-
-    $(".sample").each(function(i, element) {
-        var slug = element.getAttribute('sample').match(/([^#]+)(?:#(.+))?/);
-        var file = slug[1], sampleName = slug[2];
-
-        $.ajax({url: file, success: function(code) {
-            var sample = null;
-
-            if (sampleName === undefined) {
-                sample = code;
-            } else {
-                var sampleRegexp = new RegExp(
-                    // match 'sample(sampleName)'
-                    /sample\(/.source + escapeForRegexp(sampleName) + /\)[^\n]*\n/.source +
-                    // match anything in between
-                    /^([\s\S]*?)/.source +
-                    // match 'end-sample'
-                    /^[^\n]*end-sample/.source, 'mg');
-                var match = null;
-                while ((match = sampleRegexp.exec(code)) !== null) {
-                    sample = (sample || "") + match[1];
+    var fetch = function(url, done) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = (function(xhr) {
+            return function() {
+                if (xhr.readyState === 4) {
+                    if (
+                        ( xhr.status >= 200 && xhr.status < 300 ) ||
+                        ( xhr.status === 0 && xhr.responseText !== '')
+                    ) {
+                       done(xhr.responseText);
+                    }
                 }
-            }
+            };
+        })(xhr);
+        xhr.open( "GET", url);
+        try {
+            xhr.send();
+        }
+        catch ( e ) {
+            console.log('Failed to get the file ' + url);
+        }
+    };
 
-            // Strip trailing newline in the sample (if any), since that is
-            // only required to insert the 'end-sample' tag.
-            sample = sample.replace(/\n$/, "");
+    var slug;
+    var elements = document.querySelectorAll('.sample');
+    for (var i = 0, c= elements.length; i < c; i++) {
+        slug = elements[i].getAttribute('sample').match(/([^#]+)(?:#(.+))?/);
+        fetch(
+            slug[1],
+            function(element, file, sampleName) {
+                return function(code) {
+                    var sample = null;
 
-            if (sample === null) {
-                throw "Could not find sample '" + sampleName + "' in file '" + file + "'.";
-            }
+                    if (sampleName === undefined) {
+                        sample = code;
+                    } else {
+                        var sampleRegexp = new RegExp(
+                            // match 'sample(sampleName)'
+                            /sample\(/.source + escapeForRegexp(sampleName) + /\)[^\n]*\n/.source +
+                            // match anything in between
+                            /^([\s\S]*?)/.source +
+                            // match 'end-sample'
+                            /^[^\n]*end-sample/.source, 'mg');
+                        var match = null;
+                        while ((match = sampleRegexp.exec(code)) !== null) {
+                            sample = (sample || "") + match[1];
+                        }
+                    }
 
-            var extension = file.split('.').pop();
-            $(element).text(sample);
-            if (!$(element).is('[class*="language-"]') && !$(element).is('[class*="lang-"]')) {
-                $(element).addClass("language-" + extension);
-            }
-            if (typeof hljs != 'undefined') {
-                hljs.highlightBlock(element);
-            }
-        }});
-    });
+                    // Strip trailing newline in the sample (if any), since that is
+                    // only required to insert the 'end-sample' tag.
+                    sample = sample.replace(/\n$/, "");
+
+                    if (sample === null) {
+                        throw "Could not find sample '" + sampleName + "' in file '" + file + "'.";
+                    }
+
+                    var extension = file.split('.').pop();
+                    element.textContent = sample;
+                    var classString = element.getAttribute('class') || '';
+                    if (!classString.match(/(^|\s)lang(uage)?-/)) {
+                        element.setAttribute('class', classString + ' language-' + extension);
+                    }
+                    if (typeof hljs !== 'undefined') {
+                        hljs.highlightBlock(element);
+                    }
+                }
+            }(elements[i], slug[1], slug[2])
+        );
+    }
 })();
